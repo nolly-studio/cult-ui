cb384a0aa65657c54369a435152efb3c045dc36d
Jordan Gilliam <gilliamja.te@gmail.com>
2025-10-22 18:15:24 -0400

    feat: new aisdk agents announcment

--- apps/www/components/announcement.tsx
+++ apps/www/components/announcement.tsx
@@ -1,6 +1,11 @@
 import Link from "next/link"
 import { ArrowRightIcon } from "@radix-ui/react-icons"
-import { YoutubeIcon } from "lucide-react"
+import {
+  ArrowRight,
+  ExternalLink,
+  SparklesIcon,
+  YoutubeIcon,
+} from "lucide-react"
 
 import { Separator } from "@/components/ui/separator"
 import { NeumorphEyebrow } from "@/registry/default/ui/neumorph-eyebrow"
@@ -8,16 +13,44 @@ import { NeumorphEyebrow } from "@/registry/default/ui/neumorph-eyebrow"
 export function Announcement() {
   return (
     <Link
-      href="https://pro.cult-ui.com/templates/cult-directory-template"
+      href="https://aisdkagents.com"
       className="hidden md:block "
       target="_blank"
       rel="noreferrer"
     >
       <NeumorphEyebrow className="space-x-2">
-        <YoutubeIcon className="size-4 group-hover:rotate-6 group-hover:fill-cyan-300" />{" "}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          width={24}
+          height={24}
+          color={"#000000"}
+          fill={"none"}
+          className="group-hover:rotate-6 group-hover:fill-cyan-300 size-4"
+        >
+          <title>Sparkles Icon</title>
+          <path
+            opacity="0.4"
+            d="M15 2L15.5387 4.39157C15.9957 6.42015 17.5798 8.00431 19.6084 8.46127L22 9L19.6084 9.53873C17.5798 9.99569 15.9957 11.5798 15.5387 13.6084L15 16L14.4613 13.6084C14.0043 11.5798 12.4202 9.99569 10.3916 9.53873L8 9L10.3916 8.46127C12.4201 8.00431 14.0043 6.42015 14.4613 4.39158L15 2Z"
+            fill="#141B34"
+          />
+          <path
+            d="M15 2L15.5387 4.39157C15.9957 6.42015 17.5798 8.00431 19.6084 8.46127L22 9L19.6084 9.53873C17.5798 9.99569 15.9957 11.5798 15.5387 13.6084L15 16L14.4613 13.6084C14.0043 11.5798 12.4202 9.99569 10.3916 9.53873L8 9L10.3916 8.46127C12.4201 8.00431 14.0043 6.42015 14.4613 4.39158L15 2Z"
+            stroke="#141B34"
+            strokeWidth="1.5"
+            strokeLinejoin="round"
+          />
+          <path
+            d="M7 12L7.38481 13.7083C7.71121 15.1572 8.84275 16.2888 10.2917 16.6152L12 17L10.2917 17.3848C8.84275 17.7112 7.71121 18.8427 7.38481 20.2917L7 22L6.61519 20.2917C6.28879 18.8427 5.15725 17.7112 3.70827 17.3848L2 17L3.70827 16.6152C5.15725 16.2888 6.28879 15.1573 6.61519 13.7083L7 12Z"
+            stroke="#141B34"
+            strokeWidth="1.5"
+            strokeLinejoin="round"
+          />
+        </svg>
+        {/* <SparklesIcon className="size-4 group-hover:rotate-6 group-hover:fill-cyan-300" />{" "} */}
         <Separator className="mx-2 h-4 bg-border/80" orientation="vertical" />{" "}
-        <span>New! nextjs directory template</span>
-        <ArrowRightIcon className="ml-1 size-4" />
+        <span>New! AI SDK agent patterns</span>
+        <ExternalLink className="size-3" />
       </NeumorphEyebrow>
     </Link>
   )
